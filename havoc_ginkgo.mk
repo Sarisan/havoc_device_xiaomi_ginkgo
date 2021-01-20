fcffc04dc7af195d71c67c9130015ac4ed8e6878
Sarisan <sekoohaka.sarisan@gmail.com>
2021-01-20 17:26:39 +0300

    ginkgo: Don't include ARCore

--- havoc_ginkgo.mk
+++ havoc_ginkgo.mk
@@ -24,6 +24,7 @@ $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
 # Inherit some common Havoc-OS stuff
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
 $(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Device identifier
